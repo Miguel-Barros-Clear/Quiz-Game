0380a558b4b4995990254aaffd37412101e1b8d2
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-05-11 10:01:44 -0300

    feat: adding register user integration with API

--- quiz-game-app/src/app/login/page.tsx
+++ quiz-game-app/src/app/login/page.tsx
@@ -4,6 +4,8 @@ import React, { useState } from 'react';
 import Image from 'next/image'
 import Link from 'next/link';
 
+import ApiController from '@/controllers/apiController';
+
 export default function Login() {
 
   const [userData, setUserData]: any = useState({
@@ -23,6 +25,8 @@ export default function Login() {
       placeholder: '*********',
     },
   ]
+
+  const API = new ApiController();
 
   return (
     <main className='container flex w-screen h-screen'>
@@ -44,7 +48,7 @@ export default function Login() {
                   }} value={userData[input.label.toLowerCase()]} />
                 </div>
               ))}
-              <svg className='mt-1 w-14 self-center cursor-pointer transition-all ease-in-out hover:ml-5' width="74" height="74" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg onClick={() => API.login(userData.username, userData.password)} className='mt-1 w-14 self-center cursor-pointer transition-all ease-in-out hover:ml-5' width="74" height="74" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M37 73.5C31.9162 73.5 27.1388 72.5347 22.6675 70.6041C18.1962 68.6735 14.3069 66.0557 10.9994 62.7506C7.69187 59.4431 5.07405 55.5537 3.1459 51.0825C1.21775 46.6112 0.25245 41.8337 0.25 36.75C0.25 31.6662 1.2153 26.8888 3.1459 22.4175C5.0765 17.9462 7.69432 14.0569 10.9994 10.7494C14.3069 7.44187 18.1962 4.82405 22.6675 2.8959C27.1388 0.96775 31.9162 0.00245 37 0C42.0837 0 46.8612 0.9653 51.3325 2.8959C55.8037 4.8265 59.6931 7.44432 63.0006 10.7494C66.3081 14.0569 68.9272 17.9462 70.8578 22.4175C72.7884 26.8888 73.7524 31.6662 73.75 36.75C73.75 41.8337 72.7847 46.6112 70.8541 51.0825C68.9235 55.5537 66.3057 59.4431 63.0006 62.7506C59.6931 66.0581 55.8037 68.6772 51.3325 70.6078C46.8612 72.5384 42.0837 73.5024 37 73.5ZM37.735 40.425L34.4275 43.7325C33.7537 44.4062 33.4169 45.2637 33.4169 46.305C33.4169 47.3462 33.7537 48.2037 34.4275 48.8775C35.1012 49.5512 35.9587 49.8881 37 49.8881C38.0412 49.8881 38.8987 49.5512 39.5725 48.8775L49.1275 39.3225C49.8625 38.5875 50.23 37.73 50.23 36.75C50.23 35.77 49.8625 34.9125 49.1275 34.1775L39.5725 24.6225C38.8987 23.9487 38.0412 23.6119 37 23.6119C35.9587 23.6119 35.1012 23.9487 34.4275 24.6225C33.7537 25.2962 33.4169 26.1537 33.4169 27.195C33.4169 28.2362 33.7537 29.0938 34.4275 29.7675L37.735 33.075H25.975C24.9337 33.075 24.0603 33.4278 23.3547 34.1334C22.6491 34.839 22.2975 35.7112 22.3 36.75C22.3 37.7912 22.6528 38.6647 23.3584 39.3703C24.064 40.0759 24.9362 40.4274 25.975 40.425H37.735Z" fill="#8663AA" />
               </svg>
               <p className='text-center text-sm font-medium'>To enter</p>
---
+++ quiz-game-app/src/controllers/apiController.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const API_URL = 'http://localhost:3000/';
+
+export default class ApiController {
+  async login(username: string, password: string) {
+    try {
+      const response = await axios.post(API_URL + 'login', {
+        username,
+        password
+      });
+      return response.data;
+    }
+    catch (err: any) {
+      return err
+    }
+  }
+
+  async register(userData: Object) {
+    try {
+      const response = await axios.post(API_URL + 'signup', userData);
+      return response.data;
+    }
+    catch (err: any) {
+      return err
+    }
+  }
+}
