b57e6984239981b1237f382d390b58b2c26a8257
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-05-15 13:42:32 -0300

    fix: adding a new port into back and front end

--- quiz-game-app/src/controllers/apiController.ts
+++ quiz-game-app/src/controllers/apiController.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost:3000/';
+const API_URL = 'http://localhost:3333/';
 
 export default class ApiController {
   async login(username: string, password: string) {
